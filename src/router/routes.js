2fab81565bee5fd360aa729a1917a5f771a4002d
Chamnan Longlum <chamnan@irainnovation.com>
2019-07-30 16:58:34 +0700

    fix

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,21 @@
-const routes = [
-  {
+const routes = [{
     path: "/",
-    component: () => import("layouts/MyLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Index.vue") }]
-  }
-];
+    component: () =>
+        import ("layouts/MyLayout.vue"),
+    children: [{
+        path: "",
+        component: () =>
+            import ("pages/Index.vue")
+    }]
+}];
 
 // Always leave this as last one
 if (process.env.MODE !== "ssr") {
-  routes.push({
-    path: "*",
-    component: () => import("pages/Error404.vue")
-  });
+    routes.push({
+        path: "*",
+        component: () =>
+            import ("pages/Error404.vue")
+    });
 }
 
 export default routes;
